b832dccdeaaeebf43a34e3f3c64f40c35543ae7e
Vinay Phadnis <phadnisvinay30@gmail.com>
2018-06-07 22:45:19 +0530

    code cleaning

--- script/pose-analyser.js
+++ script/pose-analyser.js
@@ -7,10 +7,22 @@ var imageElement = document.getElementById('cat');
 posenet.load().then(function(net){
   return net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride)
 }).then(function(pose){
-const rightHand = pose["keypoints"][8];
-const leftHand = pose["keypoints"][7];
-const comparator = (parseFloat(pose["keypoints"][5]["position"]["y"]) + parseFloat(pose["keypoints"][6]["position"]["y"]))/2;
-//console.log(rightHand);
-console.log(pose);
-console.log(comparator);
+  analyse(pose["keypoints"]);
 })
+
+
+function analyse(imageData){
+  var relativeRight = false;
+  var relativeLeft = false;
+  const rightHand = parseFloat(imageData[8]["position"]["y"]);
+  const leftHand = parseFloat(imageData[7]["position"]["y"]);
+  const comparator = (parseFloat(imageData[5]["position"]["y"]) + parseFloat(imageData[6]["position"]["y"]))/2;
+  if (rightHand < comparator) {
+    relativeRight = true;
+  }
+  if (leftHand < comparator) {
+    relativeLeft = true;
+  }
+  console.log(`Current Value\nLeft Hand Raised: ${relativeLeft}\nRight Hand Raised: ${relativeRight}`);
+  console.log(imageData);
+}
